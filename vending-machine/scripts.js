e1c56fcc52872b9b47a9a982f7175379d63af63e
unknown <jon.borchardt1992@gmail.com>
2023-01-05 16:52:34 -0600

    committing veneding machine progress that includes change return functionality

--- vending-machine/scripts.js
+++ vending-machine/scripts.js
@@ -69,12 +69,14 @@ function requestVend(id){
     
     if(item === undefined){
         currentMessage = `Please select an Item first! I recommend ${inventory[getRndInteger(1, inventory.length)].name}`
+        document.getElementById("foodPlease").play();
     }
     else if(item.quantity === 0){
         currentMessage = `Sorry, ${item.name} is out of stock.`
     }
     else if(currentMoney < item.price){
         currentMessage = `$ ${(item.price).toFixed(2)} is needed for this item, please enter more money.`;
+        document.getElementById("goldPlease").play();
     }
     else{
         item.quantity--;
@@ -90,6 +92,7 @@ function requestVend(id){
 function addDollar(){
     document.getElementById("dollarInsert").play();
     currentMoney += 1.00;
+    currentMoney = Math.round(currentMoney*100)/100
     document.getElementById("currentMoneyDisplay").innerHTML = "$" + (Math.round(currentMoney*100)/100).toFixed(2);
     currentMessage = "Dollar added! Big spender!";
     displayMessage(currentMessage);
@@ -97,6 +100,7 @@ function addDollar(){
 function addQuarter(){
     document.getElementById("coinInsert").play();
     currentMoney += 0.25;
+    currentMoney = Math.round(currentMoney*100)/100
     document.getElementById("currentMoneyDisplay").innerHTML = "$" + (Math.round(currentMoney*100)/100).toFixed(2);
     currentMessage = "Quarter added! That's a gumball's worth!";
     displayMessage(currentMessage);
@@ -104,6 +108,7 @@ function addQuarter(){
 function addDime(){
     document.getElementById("coinInsert").play();
     currentMoney += 0.10;
+    currentMoney = Math.round(currentMoney*100)/100
     document.getElementById("currentMoneyDisplay").innerHTML = "$" + (Math.round(currentMoney*100)/100).toFixed(2);
     currentMessage = "Dime added! Not even a gumball's worth!";
     displayMessage(currentMessage);
@@ -111,6 +116,7 @@ function addDime(){
 function addNickel(){
     document.getElementById("coinInsert").play();
     currentMoney += 0.05;
+    currentMoney = Math.round(currentMoney*100)/100
     document.getElementById("currentMoneyDisplay").innerHTML = "$" + (Math.round(currentMoney*100)/100).toFixed(2);
     currentMessage = "Nickel added! You've got a long way to go, kid!";
     displayMessage(currentMessage);
@@ -142,5 +148,46 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
   }
 function returnChange(){
-    document.getElementById("coinReturn").play();
+    resetChangeDisplays()
+    if(currentMoney>0){
+        document.getElementById("coinReturn").play();
+        displayChange();
+        document.getElementById("currentMoneyDisplay").innerHTML = "$" + (Math.round(currentMoney*100)/100).toFixed(2);
+    }
+    else{
+        document.getElementById("goldPlease").play();
+    }
+}
+function displayChange(){
+    
+    if(currentMoney>=1){
+        var totalDollars = Math.floor((currentMoney/1));
+        currentMoney -= totalDollars;
+        currentMoney = Math.round(currentMoney*100)/100;
+        document.getElementById("dollarChangeDisplay").innerHTML = totalDollars;
+    }
+    if(currentMoney>=.25){
+        var totalQuarters = Math.floor((currentMoney/.25));
+        currentMoney -= (totalQuarters*.25).toFixed(2);
+        currentMoney = Math.round(currentMoney*100)/100
+        document.getElementById("quarterChangeDisplay").innerHTML = totalQuarters;
+    }
+    if(currentMoney>=.10){
+        var totalDimes = Math.floor((currentMoney/.10));
+        currentMoney -= (totalDimes*.10).toFixed(2);
+        currentMoney = Math.round(currentMoney*100)/100;
+        document.getElementById("dimeChangeDisplay").innerHTML = totalDimes;
+    }
+    if(currentMoney>=.05){
+        var totalNickels = Math.floor((currentMoney/.05));
+        currentMoney -= (totalNickels*.05).toFixed(2);
+        currentMoney = Math.round(currentMoney*100)/100;
+        document.getElementById("nickelChangeDisplay").innerHTML = totalNickels;
+    }
+}
+function resetChangeDisplays(){
+    document.getElementById("dollarChangeDisplay").innerHTML = 0;
+    document.getElementById("quarterChangeDisplay").innerHTML = 0;
+    document.getElementById("dimeChangeDisplay").innerHTML = 0;
+    document.getElementById("nickelChangeDisplay").innerHTML = 0;
 }
